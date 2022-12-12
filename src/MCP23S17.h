ecd9b94300f022847e1fbd1bafe5a9ff1bcbd359
Ivan Labáth <ivan.git@monoid.sk>
2022-12-12 17:07:44 +0100

    refactor 16-bit cached data access

--- src/MCP23S17.h
+++ src/MCP23S17.h
@@ -96,9 +96,10 @@ class MCP23S17 {
         /* Read register and return value (1 byte) */
         uint8_t readRegister8(uint8_t addr) { readRegister(addr, 1); return _reg[addr]; }
         /* Read register and return value (2 bytes, little-endian) */
+        uint16_t getRegister16(uint8_t addr) { return (((uint16_t) _reg[addr + 1]) << 8) | _reg[addr]; }
         uint16_t readRegister16(uint8_t addr) {
             readRegister(addr, 2);
-            return (((uint16_t) _reg[addr + 1]) << 8) | _reg[addr];
+            return getRegister16(addr);
         }
         void writeRegister(uint8_t addr, uint8_t size = 1);
         void readAll();
